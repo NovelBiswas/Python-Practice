7e802b53599532f1fd3a82a11023ac588b04af8b
Novel Biswas <78224453+NovelBiswas@users.noreply.github.com>
2026-03-29 19:43:35 +0530

    Bitwise

--- Task1/Q4.py
+++ Task1/Q4.py
@@ -5,7 +5,6 @@ b = int(input("Enter b: "))
 # Bitwise XOR
 xor_result = a ^ b
 print(f"{a} ^ {b} = {xor_result} (Binary: {bin(xor_result)})")
-
 # Bitwise NOT
 not_a = ~a
 print(f"~{a} = {not_a} (Binary: {bin(not_a)})")
