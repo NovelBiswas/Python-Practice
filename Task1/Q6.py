af39e4630645d08061d62a02c20860c482b07577
Novel Biswas <78224453+NovelBiswas@users.noreply.github.com>
2026-03-30 21:02:01 +0530

    Refactor comments and maintain functionality

--- Task1/Q6.py
+++ Task1/Q6.py
@@ -1,6 +1,5 @@
 #Q6) Program: find largest number out of given three numbers ie., a, b & c. 
 #(you can assign value to a, b and c as per your choice) 
-
 A = int(input("Enter A: "))
 B = int(input("Enter B: ")) 
 C = int(input("Enter C: "))
